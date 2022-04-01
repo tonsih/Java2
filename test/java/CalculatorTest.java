8d8ae9ad10373db8be31ad07d352fb93819a82a7
Toni Sihvola <svenne.svensson@posteo.se>
2022-04-01 16:54:04 +0200

    init

--- test/java/CalculatorTest.java
+++ test/java/CalculatorTest.java
@@ -6,17 +6,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
-    @DisplayName("2 + 1 = 3") void addsTwoNumbers() {
+    @DisplayName("2 + 1 = 3")
+    void addsTwoNumbers() {
         Calculator calculator = new Calculator();
         assertEquals(3, calculator.add(2, 1),
                 "2+1 should equal 3");
     }
 
     @Test
-    @DisplayName("2 - 1 = 1") void subtractsTwoNumbers() {
+    @DisplayName("2 - 1 = 1")
+    void subtractsTwoNumbers() {
         Calculator calculator = new Calculator();
         assertEquals(1, calculator.subtract(2,1),
                 "2-1 should equal 1");
     }
 
+    @Test
+    @DisplayName("2 * 1 = 2")
+    void multiplyTwoNumbers() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.multiply(2,1),
+                "2*1 should equal 1");
+    }
 }
