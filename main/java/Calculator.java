8135660d35f7e4ca0d18dfa3660f1aacf6966f8d
Toni Sihvola <svenne.svensson@posteo.se>
2022-04-02 22:07:59 +0200

    added spacing

--- main/java/Calculator.java
+++ main/java/Calculator.java
@@ -1,15 +1,37 @@
 public class Calculator {
 
-    public int add(int a, int b) {
-        return a+b;
+    public int add(int a,
+                   int b) {
+        return a + b;
     }
 
-    public int subtract(int a, int b) {
-        return a-b;
+    public int subtract(int a,
+                        int b) {
+        return a - b;
     }
 
-    public int multiply(int a, int b) {
-        return a*b;
+    public int multiply(int a,
+                        int b) {
+        return a * b;
     }
+
+    public int division(int a,
+                        int b) {
+        return a / b;
+    }
+
+    public int power(int a,
+                     int b) {
+        if (b == 0) {
+            // return 1 -- Correct result
+            return 0;   // Should be 1 - otherwise eg: a * a * a * 0  = 0
+        }
+        return a * (power(a, b - 1));
+    }
+
+    public boolean isInt(double a) {
+        return a == (int) a;
+    }
+
 
 }
--- test/java/CalculatorTest.java
+++ test/java/CalculatorTest.java
@@ -1,32 +1,79 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
-    @Test
-    @DisplayName("2 + 1 = 3")
-    void addsTwoNumbers() {
-        Calculator calculator = new Calculator();
-        assertEquals(3, calculator.add(2, 1),
-                "2+1 should equal 3");
+    private final Calculator calculator = new Calculator();
+
+    @Nested
+    class typeChecks {
+
+        @ParameterizedTest  // Loops through all elements in the @ValueSource
+        @ValueSource(doubles = {5.0, 4.0, 13.0, 4.5}) void isInt(double i) {
+            assertTrue(calculator.isInt(i)); // A bug on purpose in 4.5
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {"4.0, true", "5.0, true", "4.8, false"})
+        void isInt2(double j,
+                    boolean k) {
+            assertEquals(k, calculator.isInt(j));
+        }
+
+
+        @Test void isNull() {
+            Integer i = null;
+
+            assertThrows(NullPointerException.class,
+
+                    () -> {
+                        i.intValue();
+                    }
+
+            );
+        }
+
     }
 
-    @Test
-    @DisplayName("2 - 1 = 1")
-    void subtractsTwoNumbers() {
-        Calculator calculator = new Calculator();
-        assertEquals(1, calculator.subtract(2,1),
-                "2-1 should equal 1");
-    }
 
-    @Test
-    @DisplayName("2 * 1 = 2")
-    void multiplyTwoNumbers() {
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.multiply(2,1),
-                "2*1 should equal 1");
-    }
+    @Nested
+    class Calculations {
 
+        @Test
+        @DisplayName("2 + 1 = 3")
+        void addsTwoNumbers() {
+            assertEquals(3, calculator.add(2, 1), "2+1 should equal 3");
+        }
+
+        @Test
+        @DisplayName("2 - 1 = 1")
+        void subtractsTwoNumbers() {
+            assertEquals(1, calculator.subtract(2, 1), "2-1 should equal 1");
+        }
+
+        @Test
+        @DisplayName("3 * 2 = 6")
+        void multiplyTwoNumbers() {
+            assertEquals(6, calculator.multiply(3, 2), "2*1 should equal 1");
+        }
+
+        @Test
+        @DisplayName("4 / 2 = 2")
+        void divideTwoNumber() {
+            assertEquals(2, calculator.division(4, 2));
+        }
+
+        @Test
+        @DisplayName("2 ^ 3 = 8")
+        void power() {
+            assertEquals(8, calculator.power(2, 3));
+        }
+
+    }
 }
